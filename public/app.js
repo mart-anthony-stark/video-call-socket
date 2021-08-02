9e32a4f3ac6b617d5f9c28f39aefbee86ee3c3f6
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-08-02 12:20:48 +0000

    video stream

--- public/app.js
+++ public/app.js
@@ -8,6 +8,8 @@ const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream)
 })
 
 socket.emit('join-room', {room: ROOM_ID, userId: 10})
@@ -19,3 +21,16 @@ socket.on('user-connected', (user) => {
 socket.on('user-disconnected', (user) => {
   console.log(`${user} disconnected...`)
 })
+
+
+
+
+
+
+function addVideoStream(video, stream){
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', ()=> {
+    video.play()
+  })
+  .append(video)
+}
