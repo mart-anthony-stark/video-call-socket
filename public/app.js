405a6575abe5444ded5e8ae7e925b5067d187e9d
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-08-02 12:08:17 +0000

    navigator peer js

--- public/app.js
+++ public/app.js
@@ -1,6 +1,14 @@
 const socket = io()
 
 const myPeer = new Peer()
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+})
 
 socket.emit('join-room', {room: ROOM_ID, userId: 10})
 
